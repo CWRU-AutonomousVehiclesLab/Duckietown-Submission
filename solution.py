8ae268627442bc1f3a79fa364c6f4ad00dc1fde8
frank-qcd-qk <cxq41@case.edu>
2019-12-05 10:31:58 -0500

    updated net dependency

--- solution.py
+++ solution.py
@@ -17,10 +17,38 @@ convertion_wrapper = SteeringToWheelVelWrapper()
 
 
 class TensorflowTemplateAgent:
+    #-----------------------------------------------------------------------------
+    # Define custom loss functions for regression in Keras 
+    #-----------------------------------------------------------------------------
+
+    # root mean squared error (rmse) for regression
+    def rmse(self,y_true, y_pred):
+        from keras import backend
+        return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+
+    # mean squared error (mse) for regression
+    def mse(self,y_true, y_pred):
+        from keras import backend
+        return backend.mean(backend.square(y_pred - y_true), axis=-1)
+
+    # coefficient of determination (R^2) for regression
+    def r_square(self,y_true, y_pred):
+        from keras import backend as K
+        SS_res =  K.sum(K.square(y_true - y_pred)) 
+        SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+        return (1 - SS_res/(SS_tot + K.epsilon()))
+
+    def r_square_loss(self,y_true, y_pred):
+        from keras import backend as K
+        SS_res =  K.sum(K.square(y_true - y_pred)) 
+        SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+        return 1 - ( 1 - SS_res/(SS_tot + K.epsilon()))
+
     def __init__(self):
 
         # define observation and output shapes
-        self.model = load_model("FrankNet.h5")
+        self.dependencies={'rmse':self.rmse,'mse':self.mse,'r_square':self.r_square,'r_square_loss':self.r_square_loss}
+        self.model = load_model("FrankNet.h5",custom_objects=self.dependencies)
 
         self.current_image = np.zeros(expect_shape)
         self.input_image = np.zeros((100, 200, 3))
@@ -31,6 +59,7 @@ class TensorflowTemplateAgent:
 
     def init(self, context: Context):
         context.info('init()')
+
 
     def on_received_seed(self, data: int):
         np.random.seed(data)
